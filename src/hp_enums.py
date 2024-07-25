76bc1ef5f0d4fd1ad39f5913fb14ea2a50e32a7a
Lesko <polensek.matevz@gmail.com>
2024-07-25 06:13:36 +0200

    Started adding energy consumption functionality.

--- src/hp_enums.py
+++ src/hp_enums.py
@@ -17,8 +17,13 @@ class APIEndpoint(Enum):
     SYSTEM_REVIEW = "TopPage=1&Subpage=2"
     HEATING_LOOP_2 = "TopPage=1&Subpage=6"
     TIMELINE_GRAPH = "TopPage=4&Subpage=1&Action=3"
+    CONSUMPTION_HISTOGRAM = "TopPage=4&Subpage=4&Action=4"
 
 
 class TimelineGraphRequestData(Enum):
     INVERTER_FREQUENCY = 27
     ELECTRIC_CONSUMPTION = 29
+
+
+class ConsumptionHistogramData(Enum):
+    CONSUMPTION = 17  # TODO: not correct!
