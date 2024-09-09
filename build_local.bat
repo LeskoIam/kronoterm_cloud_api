8c3fb4c80d23af59e20b081928165352b565c73e
Lesko <polensek.matevz@gmail.com>
2024-09-09 15:24:27 +0200

    Split requirements.txt into dev and prod.

--- build_local.bat
+++ build_local.bat
@@ -2,7 +2,9 @@ rm dist/*
 
 pydoc-markdown -I src --render-toc > docs\kronoterm_cloud_api_docs.md
 git add docs\kronoterm_cloud_api_docs.md
-git commit -m "Updated kronoterm_cloud_api_docs.md"
+git commit docs\kronoterm_cloud_api_docs.md -m "Updated kronoterm_cloud_api_docs.md"
+
+python -m pip install -U -r requirements.txt
 
 python -m build
 
