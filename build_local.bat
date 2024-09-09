b9aad2ecd7f57f734d838e00bbb01975344eaae4
Lesko <polensek.matevz@gmail.com>
2024-09-09 16:00:33 +0200

    Removed commiting to git for locally run build process.

--- build_local.bat
+++ build_local.bat
@@ -1,8 +1,6 @@
 rm dist/*
 
 pydoc-markdown -I src --render-toc > docs\kronoterm_cloud_api_docs.md
-git add docs\kronoterm_cloud_api_docs.md
-git commit docs\kronoterm_cloud_api_docs.md -m "Updated kronoterm_cloud_api_docs.md"
 
 python -m pip install -U -r requirements.txt
 
