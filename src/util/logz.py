940742434470e5210061f863959e3e7dce6236cc
Lesko <matevzlesko@gmail.com>
2024-09-18 08:52:17 +0200

    Improved logging.

---
+++ src/util/logz.py
@@ -0,0 +1,19 @@
+import logging
+import logging.handlers
+import os
+
+LOG_FILENAME = "kronoterm_cloud_api.log"
+
+
+def create_logger():
+    """Create a logger for use in all cases."""
+    loglevel = os.environ.get("LOGLEVEL", "INFO").upper()
+    file_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1000000, backupCount=5)
+    console_handler = logging.StreamHandler()
+    logging.basicConfig(
+        level=loglevel,
+        format="%(asctime)s [%(levelname)8s] [%(filename)s:%(lineno)4s:%(funcName)20s()]\t%(message)s",
+        datefmt="[%d.%m.%Y %H:%M;%S]",
+        handlers=[file_handler, console_handler],
+    )
+    return logging.getLogger("rich")
